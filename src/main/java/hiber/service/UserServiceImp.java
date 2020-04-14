18177b8358b9f02cc05103a884ac9d3a35d157e8
RomanK <namor925@gmail.com>
2020-04-14 22:20:35 +0700

    first commit

---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,29 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+   //SELECT u.name, u.last_name FROM users u INNER JOIN cars c ON u.car_id = c.id WHERE c.name = "Volkswagen" AND series = "568";
+   @Autowired
+   private UserDao userDao;
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+}
