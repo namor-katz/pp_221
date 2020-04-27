010056f1d1b43d01b5eb2be1e23c0de950fe3a5c
RomanK <namor925@gmail.com>
2020-04-27 21:18:19 +0700

    delete user and add car without user

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -16,35 +16,32 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-
+      userService.add(new User("Petrovich", "Ivanov", "schwain_petr@mail.ru", new Car("Oka", 154)));
+      userService.add(new User("Ivan", "Govnov", "ivan@mail.gov", new Car("Zaz", 968)));
 
       CarService carService = context.getBean((CarService.class));
-      carService.add(new Car("ladda", 777));
       userService.add(new User("Vasja", "Pupkin", "superVasja@ya.ru", new Car("Vaz", 777)));
 
-      User userT = userService.getUserByCar("Vaz", 777);
-      System.out.println("имя владельца " + userT.getLastName() + " а мыло " + userT.getEmail());
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
          System.out.println();
       }
 
       List<Car> cars = carService.listCar();
       for (Car car : cars) {
-         System.out.println("name Car = " + car.getName());
-         System.out.println("car number = " + car.getSeries());
+         System.out.println("name Car is " + car.getName());
+         System.out.println("car number is " + car.getSeries());
          System.out.println();
       }
 
+      User userT = userService.getUserByCar("Vaz", 777);
+      System.out.println("сейчас будет показан пользователь, владеющий Vaz 777");
+      System.out.println("имя владельца " + userT.getLastName() + " а мыло " + userT.getEmail());
       context.close();
    }
 }
